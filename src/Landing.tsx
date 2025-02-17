0e67f5611019dcc523bfa1f96c64e541cdcf60e6
joelbarton-io <joelbarton406@gmail.com>
2025-02-16 20:47:44 -0700

    chore: update loader, render classes

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -7,17 +7,31 @@ type Member = {
   phonenumber: string;
   profile_type: "admin" | "client" | "instructor";
 };
+type Class = {
+  id: number;
+  category: string;
+  tag: string;
+  instructor_id: number;
+  class_date: string;
+  capacity: number;
+};
 
-export const landingLoader = async (): Promise<Member[]> => {
-  const res = await fetch(`${base_url}/members`);
-  return res.json();
+export const landingLoader = async (): Promise<[Member[], Class[]]> => {
+  const [membersResponse, classesResponse] = await Promise.all([
+    fetch(`${base_url}/members`),
+    fetch(`${base_url}/classes`),
+  ]);
+
+  const members = await membersResponse.json();
+  const classes = await classesResponse.json();
+
+  return [members, classes];
 };
 
 export default function LandingPage() {
-  const members = useLoaderData() as Member[];
-
+  const [members, classes] = useLoaderData();
   if (!members) {
-    return <div>Failed to load members.</div>;
+    return <div>Failed to load members and classes.</div>;
   }
 
   return (
@@ -26,7 +40,15 @@ export default function LandingPage() {
       <ul>
         {members.map((m) => (
           <li key={m.id}>
-            {m.firstname} - {m.phonenumber}
+            {m.firstname} | {m.phonenumber}
+          </li>
+        ))}
+      </ul>
+      <h5>Classes:</h5>
+      <ul>
+        {classes.map((c) => (
+          <li key={c.id}>
+            {c.category} | {c.tag} | {c.class_date} | {c.capacity}
           </li>
         ))}
       </ul>
--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
       {
         path: "/contact",
         element: <div>Contact</div>,
-      },  
+      },
     ],
   },
 ]);
